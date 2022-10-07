930021ae661cea2e8e8b45bd25e0802a89059626
Sammi De Guzman <sammi@sammdot.ca>
2022-10-06 20:15:56 -0700

    Update metric names

--- mccistats/metrics.py
+++ mccistats/metrics.py
@@ -3,12 +3,15 @@ from prometheus_client import Gauge
 from mccistats.constants import ADDRESSES
 
 PLAYERS_ONLINE = Gauge(
-  "players_online", "Number of players connected to the server"
+  "mcci_players_online", "Number of players connected to the server"
 )
 PLAYERS_MAX = Gauge(
-  "players_max", "Maximum number of players allowed on the server at one time"
+  "mcci_players_max",
+  "Maximum number of players allowed on the server at one time",
 )
 
-LATENCY = Gauge("latency_ms", "Latency in milliseconds to the proxy", ["proxy"])
+LATENCY = Gauge(
+  "mcci_latency_ms", "Latency in milliseconds to the proxy", ["proxy"]
+)
 
-AVAILABLE = Gauge("available", "Availability of the proxy", ["proxy"])
+AVAILABLE = Gauge("mcci_available", "Availability of the proxy", ["proxy"])
